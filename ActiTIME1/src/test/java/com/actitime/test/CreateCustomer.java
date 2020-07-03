5ab5c149dd27d1fa934f31b70408997592d135de
Lenovo <Lenovo@Pranav>
2020-07-03 08:04:06 +0530

    first commit

---
+++ ActiTIME1/src/test/java/com/actitime/test/CreateCustomer.java
@@ -0,0 +1,16 @@
+package com.actitime.test;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+
+public class CreateCustomer {
+		@Test
+		public void testCreateCustomer() {
+			Reporter.log("create customer",true);
+
+		
+		}
+}
